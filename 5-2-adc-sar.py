16e9b69c071bc5538866843ca3d0409d97cd2039
Андраманов Иван <andramanov.ia@phystech.edu>
2023-04-04 19:36:31 +0300

    1.1

--- 5-2-adc-sar.py
+++ 5-2-adc-sar.py
@@ -1,10 +1,10 @@
 import RPi.GPIO as gpio
 import time
-dac = [] # список портов в области DAC
+dac = [26, 19, 13, 6, 5, 11, 9, 10] # список портов в области DAC
 gpio.setmode(gpio.BCM)
 
-comp = #номер пина из области COMP
-troyka = #номер пина тройка-модуля
+comp = 4 #номер пина из области COMP
+troyka = 17 #номер пина тройка-модуля
 gpio.setup(dac, gpio.OUT)
 gpio.setup(comp, gpio.IN)
 gpio.setup(troyka, gpio.OUT, initial = gpio.HIGH)
@@ -13,21 +13,29 @@ def from10to2(x):
     x_new = bin(x)
     l = []
     for i in x_new:
-        l.append(i)
+        if i != 'b':
+            l.append(int(i))
     l.pop(0)
-    l.pop(0)
+
+    length = 0
+    if len(l) < 8:
+        length = 8 - len(l)
+    for i in range(length):
+        l.insert(0, 0)
     return l
 
 def adc():
     l =[128, 64, 32, 16, 8, 4, 2, 1]
     q = 0
     for x in l:
+        gpio.output(dac, from10to2(q+x))
+        time.sleep(0.01)
         compValue = gpio.input(comp)
         if compValue == 1:
             q = q + x
         else: q = q
     gpio.output(dac, from10to2(q))
-    voltage = (q / 256) * 3.3
+    voltage = (q / 256) * 3.3 * 4.7
     print(voltage)
 
 try:
